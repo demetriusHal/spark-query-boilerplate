b53fe18ea81ee0a968e090ae6d43d10336d70bc7
demetriushal <dimitrioshal@gmail.com>
2020-02-18 20:43:15 +0200

    added more functionality of the dictionary encoding

--- src/main/java/rdfparsercheckuoa/rdf_parser_check/App.java
+++ src/main/java/rdfparsercheckuoa/rdf_parser_check/App.java
@@ -27,6 +27,9 @@ public class App
     static String column_name_predicate = "predicate";
     static String column_name_object = "object";
     static String tableName = "distinct_literals";
+    static String tableName1 = "distinct_literals_fixed";
+    static String tableName2 = "dict_final";
+    static String triples_zipped = "triples_zipped";
 
     public static void main( String[] args )
     {
@@ -36,21 +39,47 @@ public class App
         //App.spark.sql(queryDropTripleTable);
         //App.spark.sql(queryDropTripleTableFixed);
 
-        runSql();
+        showTuples(args[0]);
 
 
     }
+
+    private static void encode() {
+        Dataset<Row> res = spark.sql("SELECT s FROM prost_test"+tableName1);
+    }
     
     private static void runSql() {
-        spark.sql("DROP TABLE IF EXISTS " +tableName);
-        Dataset<Row> res = spark.sql(String.format("CREATE TABLE %1$s  AS SELECT DISTINCT(s) FROM prost_test.triples"));
-        res = spark.sql(String.format("INSERT INTO TABLE %1$s  SELECT DISTINCT(o) FROM prost_test.triples"));
+        spark.sql("DROP TABLE IF EXISTS prost_test." +tableName);
+        Dataset<Row> res = spark.sql(String.format("CREATE TABLE prost_test.%1$s  AS SELECT DISTINCT(s) FROM prost_test.triples", tableName));
+        res = spark.sql(String.format("INSERT INTO TABLE prost_test.%1$s  SELECT DISTINCT(o) FROM prost_test.triples", tableName));
         res.show();
         // spark.sql("SELECT COUNT(DISTINCT s) FROM prost_test.vp_http___data_linkedeodata_eu_ontology_has_type").show();
         // spark.sql("SELECT COUNT(DISTINCT o) FROM prost_test.vp_http___data_linkedeodata_eu_ontology_has_type").show();
 
 
+    }
+    private static void showTuples(String relation) {
+        spark.sql("SELECT * from prost_test."+relation).show(1000);
 
+
+    }
+
+    private static void createDistinctTable() {
+        spark.sql(String.format("CREATE TABLE prost_test.%1$s  AS SELECT DISTINCT(s) FROM prost_test.%2$s", tableName1, tableName));
+    }
+
+    private static void countTuples(String relation) {
+        spark.sql(String.format("SELECT COUNT(s) FROM prost_test.%1$s WHERE char_length(s) > 16", tableName1)).show();
+    }
+
+    private static void addRowNumber() {
+        spark.sql(String.format("CREATE TABLE prost_test.%1$s  AS SELECT s as key, concat('dd', (row_number() over (order by s))) value FROM prost_test.%2$s", tableName2, tableName1));
+
+    }
+
+    private static void createTriplesZipped() {
+        spark.sql("DROP TABLE IF EXISTS prost_test." +triples_zipped);
+        spark.sql(String.format("CREATE TABLE prost_test.%1$s as SELECT t1.value as s,t.p,t2.value as o FROM prost_test.dict_final t1, prost_test.dict_final t2, prost_test.triples t WHERE t1.key=t.s AND t2.key=t.o",triples_zipped));
     }
 
 
